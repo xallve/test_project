e89bca8b1923d0d1fb18806ae71b503769bacade
Bogdan <prodanov1540@gmail.com>
2021-12-01 00:53:59 +0200

    commit

--- project/project/settings.py
+++ project/project/settings.py
@@ -129,3 +129,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'EXCEPTION_HANDLER': 'project.exceptions.core_exception_handler',
+    'NON_FIELD_ERRORS_KEY': 'error',
+}
