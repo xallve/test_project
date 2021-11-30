4a6816266d03bc3e2e408797d5ef1450358342d1
Bogdan <prodanov1540@gmail.com>
2021-12-01 00:45:35 +0200

    commit

--- project/myapp/urls.py
+++ project/myapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import RegistrationAPIView
+from .views import LoginAPIView, RegistrationAPIView
 
 app_name = 'myapp'
 
 urlpatterns = [
     path('users/', RegistrationAPIView.as_view()),
+    path('users/login/', LoginAPIView.as_view()),
 ]
