8672b42f90f68702e442b139a1286f2f75972b69
Bogdan <prodanov1540@gmail.com>
2021-11-30 23:30:41 +0200

    Added models

--- project/project/settings.py
+++ project/project/settings.py
@@ -27,6 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+AUTH_USER_MODEL = 'myapp.User'
+
 
 # Application definition
 
@@ -37,6 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'myapp',
+    'django_extensions',
+    'jwt',
 ]
 
 MIDDLEWARE = [
