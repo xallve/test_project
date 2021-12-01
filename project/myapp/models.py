9a7093441d0ccb631a3fc716d250973e56222356
Bogdan <prodanov1540@gmail.com>
2021-12-01 20:47:54 +0200

    commit

--- project/myapp/models.py
+++ project/myapp/models.py
@@ -78,3 +78,13 @@ class User(AbstractBaseUser, PermissionsMixin):
         }, settings.SECRET_KEY, algorithm='HS256')
 
         return token
+
+
+class Post(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=100, blank=True, default='')
+    body = models.TextField(blank=True, default='')
+    owner = models.ForeignKey(User, related_name='posts', on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['created']
